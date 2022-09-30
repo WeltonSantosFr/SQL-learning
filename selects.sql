422092e3ed2a86e26d09bf06c15b24b13a41b7a7
=Welton Santos <weltonsantosfr@gmail.com>
2022-09-30 11:11:53 -0300

    add tables creation and selects 1 and 2

--- selects.sql
+++ selects.sql
@@ -2,9 +2,28 @@
 -- Seleções de dados
 
 -- 1)
-
+SELECT
+  produtos_pedidos.id,
+  pedidos.status,
+  clientes.id,
+  produtos.id,
+  produtos.nome,
+  produtos.tipo,
+  produtos.preco,
+  produtos.pts_de_lealdade
+  FROM
+  produtos_pedidos
+  JOIN produtos ON produtos_pedidos.id = produtos.id
+  JOIN pedidos ON produtos_pedidos.pedido_id = pedidos.id
+  JOIN clientes ON pedidos.cliente_id = clientes.id
 -- 2)
-
+SELECT
+  produtos_pedidos.id
+FROM
+  produtos_pedidos
+  JOIN produtos ON produtos_pedidos.produto_id = produtos.id
+WHERE
+  produtos.nome ILIKE 'Fritas'
 -- 3)
 
 -- 4)
